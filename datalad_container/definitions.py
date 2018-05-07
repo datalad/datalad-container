0ad19c4eec71eb101d65e2f20ffcaba4c7110f51
Benjamin Poldrack <bpoldrack@users.noreply.github.com>
2018-05-07 19:59:55 +0200

    WIP: Implement the actual command "containers" (#2)
    * Start implementing the actual command

---
+++ datalad_container/definitions.py
@@ -0,0 +1,13 @@
+import os.path as op
+from datalad.support.constraints import EnsureStr
+
+definitions = {
+    'datalad.containers.location': {
+        'ui': ('question', {
+            'title': 'Container location',
+            'text': 'path within the dataset where to store containers'}),
+            'default': op.join(".datalad", "environments"),
+            'type': EnsureStr(),
+            'destination': 'dataset'
+    },
+}
