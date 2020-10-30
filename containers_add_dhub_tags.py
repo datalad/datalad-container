b39f377306b218c62cc8d3a66b8fa80ebe3f7df3
Yaroslav Halchenko <debian@onerussian.com>
2020-10-30 19:06:34 -0400

    ENH: make ugly short code into a pretty long spaggetti to support multiple architectures etc
    and also
    - support multiple architectures
    - request specific (based on digest) image, not just "first" one
    - add bunch of TODO comments for what todo next

--- containers_add_dhub_tags.py
+++ containers_add_dhub_tags.py
@@ -1,23 +1,30 @@
 """Feed tagged Docker Hub images to datalad-containers-add.
 
 This command takes a set of Docker Hub repositories, looks up the
-tags, and calls `datalad containers-add ... dhub://REPO:TAG`.  The
+tags, and calls `datalad containers-add ... dhub://REPO:TAG@digest`.  The
 output of datalad-container's Docker adapter is dumped to
 
-    images/REPO/DIGEST/
+    images/REPO/TAG/ARCH-DATE-SHORTDIGEST/
 
-where DIGEST is the .config.digest key of the manifest returned by
-Docker Hub.  In addition, that manifest is written to
-manifests/REPO/DIGEST.json.  In both cases, the step is skipped if the
-path is already present locally.
+where SHORTDIGEST is the first 12 characters of .config.digest key of
+the manifest returned by Docker Hub for the image for the arch which was
+uploaded on the DATE. In addition, that image record and manifest are
+written to a sattelite to that directory .image.json and .manifest.json files.
+The step of adding the image is skipped if the path is already present locally.
 """
 
 import fileinput
 import json
 import logging
 from pathlib import Path
+from pprint import pprint
 import re
 import requests
+
+from datalad.api import (
+    containers_add,
+    save,
+)
 
 lgr = logging.getLogger("containers_add_dhub_tags")
 
@@ -26,6 +33,18 @@ REGISTRY_AUTH_URL = ("https://auth.docker.io/token?service=registry.docker.io"
 REGISTRY_ENDPOINT = "https://registry-1.docker.io/v2"
 DHUB_ENDPOINT = "https://hub.docker.com/v2"
 
+# TODO: wrap it up with feeding the repositories to consider
+# or if we just make it one repository at a time, then could become CLI options
+target_architectures = '.*'
+target_tags = '.*'
+
+# TEST on busybox on just a few architectures and tags - it is tiny but has too many
+#target_architectures = '^(amd64|.*86)$'
+#target_tags = '(latest|1.32.0)'
+
+# TODO this could be a CLI option
+default_architecture = 'amd64'
+
 
 def clean_container_name(name):
     """Transform `name` for use in datalad-containers-add.
@@ -33,23 +52,17 @@ def clean_container_name(name):
     Note that, although it probably doesn't matter in practice, this
     transformation is susceptible to conflicts and ambiguity.
     """
-    if name.startswith("library/"):
-        name = name[8:]
+    if name.startswith("_/"):
+        name = name[2:]
     name = name.replace("_", "-")
+    # TODO: research feasibility to create "hierarchical" organization
+    # by using . as a separator.  Then we could have a "default"
+    # one and then various past instances in  sublevels of
+    # .version.architecture.date--shortdigest
     return re.sub(r"[^0-9a-zA-Z-]", "--", name)
 
 
-def add_container(repo, tag, digest):
-    from datalad.api import containers_add
-
-    target = Path("images", repo, digest)
-    if target.exists():
-        lgr.info("Skipping %s:%s. Already exists: %s",
-                 repo, tag, target)
-        return
-
-    name = clean_container_name(f"{repo}--{tag}")
-    url = f"dhub://{repo}:{tag}"
+def add_container(url, name, target):
     lgr.info("Adding %s as %s", url, name)
     # TODO: This would result in a commit for each image, which would
     # be good to avoid.
@@ -61,33 +74,52 @@ def add_container(repo, tag, digest):
         # Pass update=True to let the image for an existing entry
         # (particularly the one for the "latest" tag) be updated.
         update=True)
+    return name
 
 
-def write_manifest(repo, digest, manifest):
-    target = Path(f"manifests/{repo}/{digest}.json")
-    if target.exists():
-        lgr.info("Manifest already exists: %s", target)
-        return
+def write_json(target, content):
     lgr.info("Writing %s", target)
     target.parent.mkdir(parents=True, exist_ok=True)
-    target.write_text(json.dumps(manifest))
+    target.write_text(json.dumps(content))
+    return target
+
+#
+# Registry -- requires authentication to query
+#
+from contextlib import contextmanager
 
 
-def get_manifests(repo, tags):
-    resp_auth = requests.get(REGISTRY_AUTH_URL.format(repo=repo))
-    resp_auth.raise_for_status()
-    headers = {
-        "Authorization": "Bearer " + resp_auth.json()["token"],
-        "Accept": "application/vnd.docker.distribution.manifest.v2+json"}
+class RepoRegistry(object):
+    def __init__(self, repo):
+        resp_auth = requests.get(REGISTRY_AUTH_URL.format(repo=repo))
+        resp_auth.raise_for_status()
+        self.repo = repo
+        self._headers = {
+            "Authorization": "Bearer " + resp_auth.json()["token"],
+        }
 
-    for tag in tags:
-        lgr.debug("Getting manifest for %s:%s", repo, tag)
-        # TODO: Can we check with HEAD first to see if the digest
-        # matches what we have locally?
-        resp_man = requests.get(f"{REGISTRY_ENDPOINT}/{repo}/manifests/{tag}",
+    def get(self, query, headers=None):
+        headers = headers or {}
+        headers.update(self._headers)
+        resp_man = requests.get(f"{REGISTRY_ENDPOINT}/{self.repo}/{query}",
                                 headers=headers)
         resp_man.raise_for_status()
-        yield tag, resp_man.json()
+        return resp_man.json()
+
+    def get_manifest(self, reference):
+        lgr.debug("Getting manifest for %s:%s", self.repo, reference)
+        # TODO: Can we check with HEAD first to see if the digest
+        # matches what we have locally?
+        return self.get(
+            f'manifests/{reference}',
+            # return the single (first, if multiple e.g. for a reference being a tag)
+            # manifest
+            headers={"Accept": "application/vnd.docker.distribution.manifest.v2+json"}
+        )
+
+#
+# HUB -- no authentication required
+#
 
 
 def walk_pages(url):
@@ -101,10 +133,17 @@ def walk_pages(url):
         yield from data.get("results", [])
 
 
-def get_repo_tags(repo):
+def get_repo_tag_images(repo):
     url = f"{DHUB_ENDPOINT}/repositories/{repo}/tags"
     for result in walk_pages(url):
-        yield result["name"]
+        images = result["images"]
+        # there could be records with images not having been uploaded,
+        # then it seems digest is not there and 'last_pushed' is None
+        for i, image in list(enumerate(images))[::-1]:
+            if 'digest' not in image:
+                assert not image.get('last_pushed')
+                images.pop(i)
+        yield result["name"], sorted(images, key=lambda i: i['digest'])
 
 
 def get_namespace_repos(name):
@@ -148,14 +187,94 @@ def process_files(files):
         else:
             repos = [name]
 
+        target_architectures_re = re.compile(target_architectures)
+        target_tags_re = re.compile(target_tags)
         for repo in repos:
+            lgr.info("Working on %s", repo)
             try:
-                for tag, manifest in get_manifests(repo, get_repo_tags(repo)):
-                    digest = manifest["config"]["digest"]
-                    assert digest.startswith("sha256:")
-                    digest = digest[7:]
-                    write_manifest(repo, digest, manifest)
-                    add_container(repo, tag, digest)
+                registry = RepoRegistry(repo)
+                #pprint(list(zip(sorted(_all_tags['latest'], key=lambda r: r['digest']), sorted(_all_tags['1.32.0'],
+                # key=lambda r: r['digest']))))
+                tag_images = dict(get_repo_tag_images(repo))
+
+                # 'latest' tag is special in docker, it is the default one
+                # which might typically point to some other release/version.
+                # If we find that it is the case, we do not create a dedicated "latest"
+                # image/datalad container -- we just add container entry pointing to that
+                # one.  If there is no matching one -- we do get "latest"
+                latest_matching_tag = None
+                if target_tags_re.match('latest'):
+                    matching_tags = []
+                    for tag, images in tag_images.items():
+                        if tag == 'latest' or not target_tags_re.match(tag):
+                            lgr.debug("Skipping tag %(tag)s")
+                            continue
+
+                        if images == tag_images['latest']:
+                            matching_tags.append(tag)
+                    if len(matching_tags) >= 1:
+                        if len(matching_tags) > 1:
+                            lgr.info(
+                                "Multiple tags images match latest, taking the first: %s",
+                                ', '.join(matching_tags))
+                        latest_matching_tag = matching_tags[0]
+                        lgr.info("Taking %s as the one for 'latest'", latest_matching_tag)
+                else:
+                    # TODO: if there is no latest, we should at least establish the
+                    # convenient one for each tag
+                    pass
+                for tag, images in tag_images.items():
+                    if tag == 'latest' and latest_matching_tag:
+                        continue  # skip since we will handle it
+                    if not target_tags_re.match(tag):
+                        lgr.debug("Skipping tag %(tag)s")
+                        continue
+                    for image in images:
+                        architecture = image['architecture']
+                        if not target_architectures_re.match(architecture):
+                            lgr.debug("Skipping architecture %(architecture)s", image)
+                            continue
+                        manifest = registry.get_manifest(image['digest'])
+                        digest = manifest["config"]["digest"]
+                        # yoh: if I got it right, it is actual image ID we see in docker images
+                        assert digest.startswith("sha256:")
+                        digest = digest[7:]
+                        digest_short = digest[:12]  # use short version in name
+                        last_pushed = image['last_pushed']
+                        assert last_pushed.endswith('Z')
+                        # take only date
+                        last_pushed = last_pushed[:10].replace('-', '')
+                        assert len(last_pushed) == 8
+                        cleaner_repo = repo
+                        # this is how it looks on hub.docker.com URL
+                        if repo.startswith('library/'):
+                            cleaner_repo = "_/" + cleaner_repo[len('library/'):]
+                        # TODO: in case of a single architecture -- do not bother with
+                        # {architecture}
+                        image_name = (f"{cleaner_repo}/{tag}/{architecture}-{last_pushed}-{digest_short}")
+                        dl_container_name = clean_container_name(str(image_name))
+                        image_path = Path("images") / image_name
+                        url = f"dhub://{repo}:{tag}@{image['digest']}"
+                        save_paths = []
+                        if image_path.exists():
+                            lgr.info("%s already exists, skipping adding", str(image_path))
+                        else:
+                            save_paths.append(write_json(Path(str(image_path) + '.manifest.json'), manifest))
+                            save_paths.append(write_json(Path(str(image_path) + '.image.json'), image))
+                            add_container(url, dl_container_name, image_path)
+                            # TODO: either fix datalad-container for https://github.com/datalad/datalad-container/issues/98
+                            # or here, since we have manifest, we can datalad download-url, and add-archive-content
+                            # of the gzipped layers (but without untarring) - that should add datalad-archive
+                            # urls to individual layers in the "saved" version
+                            # TODO: make it in a single commit with add_container at least,
+                            # or one commit for the whole repo sweep
+                            save(path=save_paths, message=f"Added manifest and image records for {dl_container_name}")
+                        # TODO: ensure .datalad/config to have additional useful fields:
+                        #  architecture, os, and manually "updateurl" since not added for
+                        #  dhub:// ATM
+                        if tag == latest_matching_tag and architecture == default_architecture:
+                            # TODO remove section if exists, copy this one
+                            lgr.warning("Tracking of 'latest' is not yet implemented")
             except requests.HTTPError as exc:
                 lgr.warning(
                     "Failed processing %s. Skipping\n  status %s for %s",
