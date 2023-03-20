0fc76d7ba51a7d3981ba2173e87d874476212050
Austin Macdonald <austin@dartmouth.edu>
2023-03-20 09:42:38 -0400

    simplify version retrieve

--- datalad_container/extractors/_load_singularity_versions.py
+++ datalad_container/extractors/_load_singularity_versions.py
@@ -15,32 +15,20 @@ from datalad.support.external_versions import external_versions
 
 
 def __get_apptainer_version():
-    try:
-        out = WitlessRunner().run("apptainer --version", protocol=StdOutCapture)
-        version = out['stdout'].strip()
-
-    except FileNotFoundError as e:
-        return None
-
+    version = WitlessRunner().run("apptainer --version", protocol=StdOutCapture)['stdout'].strip()
     strip_str = len("apptainer version ")
     return version[strip_str:]
 
 
 def __get_singularity_version():
-    try:
-        out = WitlessRunner().run("singularity version", protocol=StdOutCapture)
-        version = out['stdout'].strip()
-    except FileNotFoundError as e:
-        return None
+    version = WitlessRunner().run("singularity version", protocol=StdOutCapture)['stdout'].strip()
 
     # It may be possible to have both apptainer and singularity installed.
     # If singularity is installed independently, the versions will not match.
     apptainer_version = external_versions["cmd:apptainer"]
-    if apptainer_version and apptainer_version != version:
-        return version
-
-    return None
-
+    if apptainer_version and apptainer_version == version:
+        return None
+    return version
 
 # Load external_versions and patch with "cmd:singularity" and "cmd:apptainer"
 external_versions.add("cmd:apptainer", func=__get_apptainer_version)
