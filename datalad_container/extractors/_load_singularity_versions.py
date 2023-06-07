1f6875f24937cb8fbb688c9661b7f61378f0f443
Yaroslav Halchenko <debian@onerussian.com>
2023-06-07 13:55:19 -0400

    BF: capture also stderr while trying for singularity or apptainer
    Otherwise user might see
    
     /bin/sh: 1: apptainer: not found

--- datalad_container/extractors/_load_singularity_versions.py
+++ datalad_container/extractors/_load_singularity_versions.py
@@ -5,22 +5,21 @@ Adds:
     - external_versions["cmd:apptainer"]
     - external_versions["cmd:singularity"]
 """
-import subprocess
 
 from datalad.cmd import (
-    StdOutCapture,
+    StdOutErrCapture,
     WitlessRunner,
 )
 from datalad.support.external_versions import external_versions
 
 
 def __get_apptainer_version():
-    version = WitlessRunner().run("apptainer --version", protocol=StdOutCapture)['stdout'].strip()
+    version = WitlessRunner().run("apptainer --version", protocol=StdOutErrCapture)['stdout'].strip()
     return version.split("apptainer version ")[1]
 
 
 def __get_singularity_version():
-    return WitlessRunner().run("singularity version", protocol=StdOutCapture)['stdout'].strip()
+    return WitlessRunner().run("singularity version", protocol=StdOutErrCapture)['stdout'].strip()
 
 
 # Load external_versions and patch with "cmd:singularity" and "cmd:apptainer"
