96f78413396c41abf6771360358cbc2bd4d16313
Yaroslav Halchenko <debian@onerussian.com>
2023-10-02 15:23:03 -0400

    Robustify installation of singularity (install libfuse2)
    This is copied from datalad-extensions

--- tools/ci/install-singularity.sh
+++ tools/ci/install-singularity.sh
@@ -3,8 +3,8 @@ set -ex -o pipefail
 release="$(curl -fsSL https://api.github.com/repos/sylabs/singularity/releases/latest | jq -r .tag_name)"
 codename="$(lsb_release -cs)"
 arch="$(dpkg --print-architecture)"
-wget -O /tmp/singularity-ce.deb https://github.com/sylabs/singularity/releases/download/$release/singularity-ce_${release#v}-${codename}_$arch.deb
+wget -O /tmp/singularity-ce.deb "https://github.com/sylabs/singularity/releases/download/$release/singularity-ce_${release#v}-${codename}_$arch.deb"
 set -x
-sudo apt-get install uidmap
+sudo apt-get install uidmap libfuse2
 sudo dpkg -i /tmp/singularity-ce.deb
 sudo apt-get install -f
