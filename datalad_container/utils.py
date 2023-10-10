8846ac052c71558f0c4d25a4819cb69c43a77293
Michael Hanke <michael.hanke@gmail.com>
2023-10-10 08:25:10 +0200

    Factor out helper to read container configuration
    This is a plain code move from `containers_list` into a standalone function.
    Added value is
    
    - reusability
    - documentation, incl. type annotation
    
    Also adds rendering of the utility module's documentation.
    
    This is a first step towards resolving #238

--- datalad_container/utils.py
+++ datalad_container/utils.py
@@ -1,4 +1,8 @@
+"""Collection of common utilities"""
+
+from datalad.distribution.dataset import Dataset
 from datalad.support.external_versions import external_versions
+
 
 def get_container_command():
     for command in ["apptainer", "singularity"]:
@@ -9,3 +13,44 @@ def get_container_command():
         raise RuntimeError("Did not find apptainer or singularity")
 
 
+def get_container_configuration(
+    ds: Dataset,
+) -> dict:
+    """Report all container-related configuration in a dataset
+
+    Such configuration is identified by the item name pattern::
+
+      datalad.containers.<container-name>.<item-name>
+
+    Parameters
+    ----------
+    ds: Dataset
+      Dataset instance to report configuration on.
+
+    Returns
+    -------
+    dict
+      Keys are the names of configured containers and values are dictionaries
+      with their respective configuration items (with the
+      ``datalad.containers.<container-name>.`` prefix removed from their
+      keys).
+    """
+    # all info is in the dataset config!
+    var_prefix = 'datalad.containers.'
+    containers = {}
+    for var, value in ds.config.items():
+        if not var.startswith(var_prefix):
+            # not an interesting variable
+            continue
+        var_comps = var[len(var_prefix):].split('.')
+        cname = var_comps[0]
+        ccfgname = '.'.join(var_comps[1:])
+        if not ccfgname:
+            continue
+
+        cinfo = containers.get(cname, {})
+        cinfo[ccfgname] = value
+
+        containers[cname] = cinfo
+
+    return containers
