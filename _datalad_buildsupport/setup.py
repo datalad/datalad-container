89aab604f97ca0b4dda4c45de1a57ca45ec9c6fe
Yaroslav O. Halchenko <debian@onerussian.com>
2025-05-17 09:14:57 -0400

    BF: use setuptools.errors.OptionError instead of now removed import of distutils.DistutilsOptionError
    setuptools.errors were introduced in 59.0.0 release.
    
    See https://github.com/pypa/setuptools/issues/5000 for more rationale
    etc.

--- _datalad_buildsupport/setup.py
+++ _datalad_buildsupport/setup.py
@@ -12,12 +12,9 @@ from os.path import (
     dirname,
     join as opj,
 )
-
-from setuptools import (
-    Command,
-    DistutilsOptionError,
-)
+from setuptools import Command
 from setuptools.config import read_configuration
+from setuptools.errors import OptionError
 
 import versioneer
 
@@ -54,11 +51,11 @@ class BuildManPage(Command):
 
     def finalize_options(self):
         if self.manpath is None:
-            raise DistutilsOptionError('\'manpath\' option is required')
+            raise OptionError('\'manpath\' option is required')
         if self.rstpath is None:
-            raise DistutilsOptionError('\'rstpath\' option is required')
+            raise OptionError('\'rstpath\' option is required')
         if self.parser is None:
-            raise DistutilsOptionError('\'parser\' option is required')
+            raise OptionError('\'parser\' option is required')
         mod_name, func_name = self.parser.split(':')
         fromlist = mod_name.split('.')
         try:
@@ -173,9 +170,9 @@ class BuildRSTExamplesFromScripts(Command):
 
     def finalize_options(self):
         if self.expath is None:
-            raise DistutilsOptionError('\'expath\' option is required')
+            raise OptionError('\'expath\' option is required')
         if self.rstpath is None:
-            raise DistutilsOptionError('\'rstpath\' option is required')
+            raise OptionError('\'rstpath\' option is required')
         self.announce('Converting example scripts')
 
     def run(self):
@@ -205,7 +202,7 @@ class BuildConfigInfo(Command):
 
     def finalize_options(self):
         if self.rstpath is None:
-            raise DistutilsOptionError('\'rstpath\' option is required')
+            raise OptionError('\'rstpath\' option is required')
         self.announce('Generating configuration documentation')
 
     def run(self):
